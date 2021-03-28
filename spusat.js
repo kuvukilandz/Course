97e504dc487e85d220ff33d82d9dc0342102a98b
renzichwan <68050501+renzichwan@users.noreply.github.com>
2021-03-28 16:04:39 +0800

    Update pusat.js

--- spusat.js
+++ spusat.js
@@ -3,7 +3,7 @@ var myDiv = document.createElement("div");
 //Set its unique ID.
 myDiv.id = 'div_id';
 
-//Add your content to the DIV
+//Add your content to the DI
 myDiv.innerHTML = "<h1>Hello World!</h1>";
 
 //Finally, append the element to the HTML body
