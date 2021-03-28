c6b67cbba9f484ad95bf9d035a9f696fb7d0d9e9
renzichwan <68050501+renzichwan@users.noreply.github.com>
2021-03-28 16:03:54 +0800

    Update pusat.js

--- pusat.js
+++ pusat.js
@@ -1,1 +1,10 @@
-alert("hacked");
+var myDiv = document.createElement("div");
+
+//Set its unique ID.
+myDiv.id = 'div_id';
+
+//Add your content to the DIV
+myDiv.innerHTML = "<h1>Hello World!</h1>";
+
+//Finally, append the element to the HTML body
+document.body.appendChild(myDiv);
